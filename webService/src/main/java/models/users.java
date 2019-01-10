361153eafeb9a227c3bd8a998ae500abe9ea87cb
Valerey <Vchernousov@medlinesoft.ru>
2019-01-10 19:02:29 +0300

    Описал таблицу account, добавил в util класс HibernateSsessionFactoryUtil, устанавливающий зависимость с файлом конфигурации hibernate.cfg.xml

--- webService/src/main/java/models/users.java
+++ webService/src/main/java/models/users.java
@@ -15,6 +15,8 @@ public class users {
     private Timestamp created_on;
     private Timestamp last_login;
 
+
+
     @Id
     @Column(name = "id", nullable = false, insertable = true, updatable = true)
     public int getId() {
@@ -25,7 +27,53 @@ public class users {
         this.id=id;
     }
 
+    @Basic
+    @Column(name = "username", nullable = false, insertable = true, updatable = true)
+    public String getUsername() {
+        return  username;
+    }
 
+    public void setUsername(String username){
+        this.username=username;
+    }
 
+    @Basic
+    @Column(name = "password", nullable = false, insertable = true, updatable = true)
+    public String getPassword(){
+        return password;
+    }
 
+    public void setPassword(String password){
+        this.password=password;
+    }
+
+    @Basic
+    @Column(name = "email", nullable = false, insertable = true, updatable = true)
+    public String getEmail(){
+        return email;
+    }
+
+    public  void setEmail(String email){
+        this.email=email;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_on", nullable = false, insertable = true, updatable = true)
+    public Timestamp getCreated_on(){
+        return created_on;
+    }
+
+    public void  setCreated_on(Timestamp created_on){
+        this.created_on=created_on;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "last_login",nullable = true, insertable = true, updatable = true)
+    public Timestamp getLast_login(){
+        return last_login;
+    }
+
+    public void setLast_login(Timestamp last_login){
+        this.last_login=last_login;
+    }
 }
---
+++ webService/src/main/java/utils/HibernateSessionFactoryUtil.java
@@ -0,0 +1,25 @@
+package utils;
+
+import models.users;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateSessionFactoryUtil {
+    private static SessionFactory sessionFactory;
+
+    public  static SessionFactory getSessionFactory(){
+        if (sessionFactory == null){
+            try {
+                Configuration configuration = new Configuration().configure();
+                configuration.addAnnotatedClass(users.class);
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+            }catch (Exception ex) {
+                System.out.print("Исключение" + ex);
+            }
+
+        }
+        return sessionFactory;
+    }
+}
